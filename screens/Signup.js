49c10adec137607d290b27b4c1a1a7e2ed96c9e4
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-15 12:03:19 -0700

    Task: Add keyboard avoidant wrapper to sign up page

--- screens/Signup.js
+++ screens/Signup.js
@@ -39,6 +39,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faEnvelope, faEye, faEyeSlash, faLock, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons'
 import { Fontisto } from '@expo/vector-icons'
 
+// Keyboard avoiding view
+import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
+
 const Signup = () => {
   const [hidePassword, setHidePassword] = useState(true);
   let [fontsLoaded] = useFonts({
@@ -52,118 +55,120 @@ const Signup = () => {
     );
   } else {
     return (
-      <StyledContainer>
-        <InnerContainer>
-          <SubTitle style={{ fontFamily: "Quicksand_700Bold" }}>Sign Up</SubTitle>
-          <Formik
-            initialValues={{ fullName: '', email: '', zipCode: '', password: '', confirmPassword: '' }}
-            onSubmit={(values) => {
-              console.log(values);
-            }}
-          >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <StyledFormArea>
-                <UserTextInput
-                  label='Full Name'
-                  icon={faUser}
-                  placeholder='Just Doe'
-                  placeholderTextColor={xandu}
-                  onChangeText={handleChange('fullName')}
-                  onBlur={handleBlur('fullName')}
-                  value={values.fullName}
-                />
+      <KeyboardAvoidingWrapper>
+        <StyledContainer>
+          <InnerContainer>
+            <SubTitle style={{ fontFamily: "Quicksand_700Bold" }}>Sign Up</SubTitle>
+            <Formik
+              initialValues={{ fullName: '', email: '', zipCode: '', password: '', confirmPassword: '' }}
+              onSubmit={(values) => {
+                console.log(values);
+              }}
+            >
+              {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <StyledFormArea>
+                  <UserTextInput
+                    label='Full Name'
+                    icon={faUser}
+                    placeholder='Just Doe'
+                    placeholderTextColor={xandu}
+                    onChangeText={handleChange('fullName')}
+                    onBlur={handleBlur('fullName')}
+                    value={values.fullName}
+                  />
 
-                <UserTextInput
-                  label='Email'
-                  icon={faEnvelope}
-                  placeholder='example@email.com'
-                  placeholderTextColor={xandu}
-                  onChangeText={handleChange('email')}
-                  onBlur={handleBlur('email')}
-                  value={values.email}
-                  keyboardType='email-address'
-                />
+                  <UserTextInput
+                    label='Email'
+                    icon={faEnvelope}
+                    placeholder='example@email.com'
+                    placeholderTextColor={xandu}
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                    value={values.email}
+                    keyboardType='email-address'
+                  />
 
-                <UserTextInput
-                  label='Zip Code'
-                  icon={faMapMarkerAlt}
-                  placeholder='######'
-                  placeholderTextColor={xandu}
-                  onChangeText={handleChange('zipCode')}
-                  onBlur={handleBlur('zipCode')}
-                  value={values.zipCode}
-                  keyboardType='numeric'
-                />
+                  <UserTextInput
+                    label='Zip Code'
+                    icon={faMapMarkerAlt}
+                    placeholder='######'
+                    placeholderTextColor={xandu}
+                    onChangeText={handleChange('zipCode')}
+                    onBlur={handleBlur('zipCode')}
+                    value={values.zipCode}
+                    keyboardType='numeric'
+                  />
 
-                <UserTextInput
-                  label='Password'
-                  icon={faLock}
-                  placeholder='**********'
-                  placeholderTextColor={xandu}
-                  onChangeText={handleChange('password')}
-                  onBlur={handleBlur('password')}
-                  value={values.password}
-                  secureTextEntry={hidePassword}
-                  isPassword={true}
-                  hidePassword={hidePassword}
-                  setHidePassword={setHidePassword}
-                  style={{ fontFamily: "Asap_600SemiBold" }}
-                />
+                  <UserTextInput
+                    label='Password'
+                    icon={faLock}
+                    placeholder='**********'
+                    placeholderTextColor={xandu}
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                    value={values.password}
+                    secureTextEntry={hidePassword}
+                    isPassword={true}
+                    hidePassword={hidePassword}
+                    setHidePassword={setHidePassword}
+                    style={{ fontFamily: "Asap_600SemiBold" }}
+                  />
 
-                <UserTextInput
-                  label='Confirm Password'
-                  icon={faLock}
-                  placeholder='**********'
-                  placeholderTextColor={xandu}
-                  onChangeText={handleChange('confirmPassword')}
-                  onBlur={handleBlur('confirmPassword')}
-                  value={values.confirmPassword}
-                  secureTextEntry={hidePassword}
-                  isPassword={true}
-                  hidePassword={hidePassword}
-                  setHidePassword={setHidePassword}
-                  style={{ fontFamily: "Asap_600SemiBold" }}
-                />
+                  <UserTextInput
+                    label='Confirm Password'
+                    icon={faLock}
+                    placeholder='**********'
+                    placeholderTextColor={xandu}
+                    onChangeText={handleChange('confirmPassword')}
+                    onBlur={handleBlur('confirmPassword')}
+                    value={values.confirmPassword}
+                    secureTextEntry={hidePassword}
+                    isPassword={true}
+                    hidePassword={hidePassword}
+                    setHidePassword={setHidePassword}
+                    style={{ fontFamily: "Asap_600SemiBold" }}
+                  />
 
-                <MsgBox>...</MsgBox>
-                <StyledButton onPress={handleSubmit}>
-                  <ButtonText style={{ fontFamily: "Asap_600SemiBold" }}>
-                    Sign In
-                  </ButtonText>
-                </StyledButton>
-                <StyledSocialsLabel>
-                  or use one of your social profiles
-                </StyledSocialsLabel>
-                <StyledSocialsContainer>
-                  <StyledButton google={true} onPress={handleSubmit}>
-                    <Fontisto name="google" color={cultured} size={25} />
-                    <ButtonText google={true} style={{ fontFamily: "Asap_600SemiBold" }}>
-                      Google
+                  <MsgBox>...</MsgBox>
+                  <StyledButton onPress={handleSubmit}>
+                    <ButtonText style={{ fontFamily: "Asap_600SemiBold" }}>
+                      Sign In
                     </ButtonText>
                   </StyledButton>
-                  <StyledButton facebook={true} onPress={handleSubmit}>
-                    <Fontisto name="facebook" color={cultured} size={25} />
-                    <ButtonText facebook={true} style={{ fontFamily: "Asap_600SemiBold" }}>
-                      Facebook
-                    </ButtonText>
-                  </StyledButton>
-                </StyledSocialsContainer>
-                <ExtraView signin={true}>
-                  <TextLinkContent style={{ fontFamily: "Asap_600SemiBold" }}>
-                    Already have an account?
-                  </TextLinkContent>
-                  <TextLink>
-                    <TextLinkContent signup={true} style={{ fontFamily: "Asap_600SemiBold" }}>
-                      Sign Up
+                  <StyledSocialsLabel>
+                    or use one of your social profiles
+                  </StyledSocialsLabel>
+                  <StyledSocialsContainer>
+                    <StyledButton google={true} onPress={handleSubmit}>
+                      <Fontisto name="google" color={cultured} size={25} />
+                      <ButtonText google={true} style={{ fontFamily: "Asap_600SemiBold" }}>
+                        Google
+                      </ButtonText>
+                    </StyledButton>
+                    <StyledButton facebook={true} onPress={handleSubmit}>
+                      <Fontisto name="facebook" color={cultured} size={25} />
+                      <ButtonText facebook={true} style={{ fontFamily: "Asap_600SemiBold" }}>
+                        Facebook
+                      </ButtonText>
+                    </StyledButton>
+                  </StyledSocialsContainer>
+                  <ExtraView signin={true}>
+                    <TextLinkContent style={{ fontFamily: "Asap_600SemiBold" }}>
+                      Already have an account?
                     </TextLinkContent>
-                  </TextLink>
-                </ExtraView>
-              </StyledFormArea>
-            )}
-          </Formik>
-        </InnerContainer>
-        <StatusBar style="auto" />
-      </StyledContainer >
+                    <TextLink>
+                      <TextLinkContent signup={true} style={{ fontFamily: "Asap_600SemiBold" }}>
+                        Sign Up
+                      </TextLinkContent>
+                    </TextLink>
+                  </ExtraView>
+                </StyledFormArea>
+              )}
+            </Formik>
+          </InnerContainer>
+          <StatusBar style="auto" />
+        </StyledContainer >
+      </KeyboardAvoidingWrapper>
     );
   }
 }
